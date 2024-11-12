148f37b176eb450cb190db20910d22abb4aaf897
utkuaraal <utkuaraal73@gmail.com>
2024-11-12 03:25:42 +0300

    project files created and cqrs with mediatr implemented -1

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string name, List<string> category, string description, string imageFile, decimal price) : IRequest<CreateProductResult>;
+    public record CreateProductResult(Guid id);
+    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
