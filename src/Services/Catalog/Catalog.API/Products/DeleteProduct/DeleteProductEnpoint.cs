cc3a1ffbd4933004fe01c05f2e38f2c7b6abd73a
utkuaraal <utkuaraal73@gmail.com>
2025-01-14 04:01:39 +0300

    DeleteProduct, GetProductByCategory and UpdateProduct added and fluent validation added

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEnpoint.cs
@@ -0,0 +1,28 @@
+﻿
+using Catalog.API.Products.CreateProduct;
+
+namespace Catalog.API.Products.DeleteProduct;
+
+//public record DeleteProductRequest();
+public record DelteProductResponse(bool IsSuccess);
+public class DeleteProductEnpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/products/{id}",
+            async (Guid id, ISender sender) => 
+            {
+                var result = await sender.Send(new DeleteProductCommand(id));
+
+                var response = result.Adapt<DelteProductResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("DeleteProduct")
+            .Produces<DelteProductResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Delete Product")
+            .WithDescription("Delete Product");
+    }
+}
